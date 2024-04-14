721ab8e218f5c56ddb3908b4237aedddc4d258ec
Dongchi Yao <78290513+Dongchi-Yao@users.noreply.github.com>
2024-04-14 14:30:45 +0400

    Update mysentences.py

--- mysentences.py
+++ mysentences.py
@@ -5,49 +5,43 @@ Created on Mon Apr 25 17:22:25 2022
 @author: smrya
 """
 
-def split_into_sentences(text):
-    import re
-    alphabets= "([A-Za-z])"
-    prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
-    suffixes = "(Inc|Ltd|Jr|Sr|Co)"
-    starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
-    acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
-    websites = "[.](com|net|org|io|gov)"
-    text = " " + text + "  "
-    text = text.replace("\n"," ")
-    text = re.sub(prefixes,"\\1<prd>",text)
-    text = re.sub(websites,"<prd>\\1",text)
-    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
-    if 'et al.' in text: text=text.replace('et al.','et al<prd>')
-    if 'Fig.' in text: text=text.replace('Fig.','Fig<prd>')
-    if 'Int.' in text: text=text.replace('Int.','Int<prd>')
+import re
 
-    
-    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
-    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
-    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
-    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
-    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
-    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
-    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
-    if "”" in text: text = text.replace(".”","”.")
-    if "\"" in text: text = text.replace(".\"","\".")
-    if "!" in text: text = text.replace("!\"","\"!")
-    if "?" in text: text = text.replace("?\"","\"?")
-    
-    #Replace some characters
-    text=text.replace(' .', '.')
-    text=text.replace('- ','')
-    text=text.replace(' -','')
-    text=text.replace('  ',' ') 
-    text=text.replace('  ',' ')
-    
-    #Create stop sign
-    text = text.replace(". ",".<stop>")
-    text = text.replace("?","?<stop>")
-    text = text.replace("!","!<stop>")
-    text = text.replace("<prd>",".")
+def split_into_sentences(text):
+    # Regular expression patterns to identify special cases in text
+    alphabets = "([A-Za-z])"
+    prefixes = r"(Mr|St|Mrs|Ms|Dr)[.]"
+    suffixes = r"(Inc|Ltd|Jr|Sr|Co)"
+    starters = r"(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+    acronyms = r"([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+    websites = r"[.](com|net|org|io|gov)"
+
+    # Preprocessing the text to deal with known edge cases
+    text = " " + text + "  "
+    text = text.replace("\n", " ")
+    text = re.sub(prefixes, r"\1<prd>", text)
+    text = re.sub(websites, r"<prd>\1", text)
+    text = re.sub(r"\s" + alphabets + r"[.] ", r" \1<prd> ", text)
+    text = re.sub(acronyms + r" " + starters, r"\1<stop> \2", text)
+    text = re.sub(alphabets + r"[.]" + alphabets + r"[.]" + alphabets + r"[.]", r"\1<prd>\2<prd>\3<prd>", text)
+    text = re.sub(alphabets + r"[.]" + alphabets + r"[.]", r"\1<prd>\2<prd>", text)
+    text = re.sub(r" " + suffixes + r"[.] " + starters, r" \1<stop> \2", text)
+    text = re.sub(r" " + suffixes + r"[.]", r" \1<prd>", text)
+    text = re.sub(r" " + alphabets + r"[.]", r" \1<prd>", text)
+
+    # Handling special punctuation within quotes
+    text = re.sub(r'\." ', r'". ', text)
+    text = re.sub(r'\?" ', r'"? ', text)
+    text = re.sub(r'!" ', r'"! ', text)
+
+    # Replace placeholder markers with actual punctuation and stops
+    text = text.replace("<prd>", ".")
+    text = text.replace(". ", ".<stop>")
+    text = text.replace("? ", "?<stop>")
+    text = text.replace("! ", "!<stop>")
+
+    # Split the text into sentences using the <stop> marker
     sentences = text.split("<stop>")
-    sentences = sentences[:-1]
-    sentences = [s.strip() for s in sentences]
+    sentences = [s.strip() for s in sentences if s.strip()]
+
     return sentences
